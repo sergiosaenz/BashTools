e2b01aff31304c98d1323a2da709b9c29b6b23db
ssaenz <ssaenz010@gmail.com>
2015-06-18 15:12:07 +0200

    Script para el despliegue local de las distintas instancias.

--- deployments/deployLocal.sh
+++ deployments/deployLocal.sh
@@ -1,16 +1,55 @@
 #!/bin/bash
 
+#Recive como parámetro ($1) la ruta del fichero modificado
+#	Hacer una copia del fichero anterior en $rootPath$1"_backup"
+#	Copiar el fichero en $rootPath$1
+#
+#
+#/home/ssaenz/tools/BashTools/deployments/prueba
+
+srcPath="/opt/deploy";
+
+rootPath="/opt/instances";
+
 main () {
 
-	if [ "$#" -eq 0 ]
+	if [ "$#" -lt 2 ]
 		then
 			showUsage;
 			exit;
 	fi
+
+	confFile=${0%/*}'/'deploy.conf;
+
+	
+	if [ -f $confFile ]
+	then
+		source $confFile;
+	fi
+	
+	dir=$rootPath${1#$srcPath};
+	backupDir=$rootPath${1#$srcPath}"_backup";
+	
+	mkdir -p $dir;
+	mkdir -p $backupDir;
+	
+	oldFile=$dir"/"$2;
+	file=$1"/"$2;
+	
+	if [ -f $oldFile ]
+	then
+		cp $oldFile $backupDir;
+	fi
+	
+	if [ -f $file ]
+	then
+		cp $file $dir;
+	fi
+	exit;
 }
 
 function showUsage {
-	echo "Your script usage description"
+	echo "./deployLocal.sh <dirPath> <filename>"
 }
 
 main $@;
